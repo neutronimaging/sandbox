75ab0a850e2b212f8c05d7f45cb72dc06f124b22
Anders Kaestner <anders.kaestner@psi.ch>
2020-05-05 09:44:02 +0200

    Test c++17 file system features.

---
+++ stl/folders/foldertests.cpp
@@ -0,0 +1,15 @@
+#include #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <filesystem>
+namespace fs = std::filesystem;
+ 
+int main()
+{
+    fs::create_directories("sandbox/1/2/a");
+    fs::create_directory("sandbox/1/2/b");
+    fs::permissions("sandbox/1/2/b", fs::perms::others_all, fs::perm_options::remove);
+    fs::create_directory("sandbox/1/2/c", "sandbox/1/2/b");
+    std::system("ls -l sandbox/1/2");
+    fs::remove_all("sandbox");
+}
