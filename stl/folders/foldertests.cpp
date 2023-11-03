1b86df2637b317faaf5fe598876f13b9534a0eb6
Anders Kaestner <anders.kaestner@psi.ch>
2023-11-03 19:54:11 +0200

    Added cmake file and a recipe to compile the folder tests.

--- stl/folders/foldertests.cpp
+++ stl/folders/foldertests.cpp
@@ -1,15 +1,69 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <filesystem>
+// Compile with:
+// clang++ foldertests.cpp --std=c++17
+
+#include <cstdio>
+#include <cstring>
+#include <filesystem> 
 namespace fs = std::filesystem;
+
+#include <fstream>
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <unistd.h>
+ 
+void demo_status(const fs::path& p, fs::file_status s)
+{
+    std::cout << p;
+    // alternative: switch(s.type()) { case fs::file_type::regular: ...}
+    if (fs::is_regular_file(s))
+        std::cout << " is a regular file\n";
+    if (fs::is_directory(s))
+        std::cout << " is a directory\n";
+    if (fs::is_block_file(s))
+        std::cout << " is a block device\n";
+    if (fs::is_character_file(s))
+        std::cout << " is a character device\n";
+    if (fs::is_fifo(s))
+        std::cout << " is a named IPC pipe\n";
+    if (fs::is_socket(s))
+        std::cout << " is a named IPC socket\n";
+    if (fs::is_symlink(s))
+        std::cout << " is a symlink\n";
+    if (!fs::exists(s))
+        std::cout << " does not exist\n";
+}
  
 int main()
 {
+    // create files of different kinds
+    fs::create_directory("sandbox");
+    fs::create_directory("sandbox/dir");
     fs::create_directories("sandbox/1/2/a");
     fs::create_directory("sandbox/1/2/b");
     fs::permissions("sandbox/1/2/b", fs::perms::others_all, fs::perm_options::remove);
     fs::create_directory("sandbox/1/2/c", "sandbox/1/2/b");
     std::system("ls -l sandbox/1/2");
+    std::ofstream{"sandbox/file"}; // create regular file
+    fs::create_symlink("file", "sandbox/symlink");
+ 
+    mkfifo("sandbox/pipe", 0644);
+    sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    std::strcpy(addr.sun_path, "sandbox/sock");
+    int fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    bind(fd, reinterpret_cast<sockaddr*>(&addr), sizeof addr);
+ 
+    // demo different status accessors
+    for (auto it{fs::directory_iterator("sandbox")}; it != fs::directory_iterator(); ++it)
+        demo_status(*it, it->symlink_status()); // use cached status from directory entry
+    demo_status("/dev/null", fs::status("/dev/null")); // direct calls to status
+    demo_status("/dev/sda", fs::status("/dev/sda"));
+    demo_status("sandbox/no", fs::status("/sandbox/no"));
+ 
+    // cleanup (prefer std::unique_ptr-based custom deleters)
+    close(fd);
     fs::remove_all("sandbox");
 }
+
